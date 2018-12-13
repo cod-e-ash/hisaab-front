571e8bb0ac47245aeeadb0ff198480b0325f1bdc
Ashish Bagaddeo <ashish@bagaddeo.com>
2018-12-13 01:09:58 +0000

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BodyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BodyComponent } from './components/body/body.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InvoicesComponent } from './components/invoices/invoices.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    BodyComponent,
+    NavbarComponent,
+    DashboardComponent,
+    InvoicesComponent,
+    ProductsComponent,
+    ClientsComponent,
+    SettingsComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
