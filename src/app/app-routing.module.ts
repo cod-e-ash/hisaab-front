c6116898420856cc8949ea599343e1ffe08de933
Ashish Bagaddeo <ashish@bagaddeo.com>
2018-12-23 17:55:19 +0000

    Routing Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BodyComponent } from './components/body/body.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ProductsComponent } from './components/products/products.component';
+
 
 const routes: Routes = [
-  {path: '', component: BodyComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'orders', component: OrdersComponent},
+  {path: 'clients/suppliers', component: ClientsComponent},
+  {path: 'clients/customers', component: ClientsComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { BodyComponent } from './components/body/body.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { InvoicesComponent } from './components/invoices/invoices.component';
+import { OrdersComponent } from './components/orders/orders.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { SettingsComponent } from './components/settings/settings.component';
@@ -20,7 +20,7 @@ import { ReportsComponent } from './components/reports/reports.component';
     BodyComponent,
     NavbarComponent,
     DashboardComponent,
-    InvoicesComponent,
+    OrdersComponent,
     ProductsComponent,
     ClientsComponent,
     SettingsComponent,
