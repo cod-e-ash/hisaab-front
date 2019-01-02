b9e003ddbe99f375d9ffe977ad2244e6569a0491
Ashish Bagaddeo <ashish@bagaddeo.com>
2019-01-02 07:20:35 +0000

    Product Navigation and search added

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,9 @@
+import { NavbarComponent } from './../navbar/navbar.component';
 import { Product } from './../../models/product.model';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
 
 @Component({
   selector: 'app-products',
@@ -9,15 +12,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
   products: Product[];
+  curPage = 0;
+  totalPages: number;
+  totalRecs: number;
+  navPages: number[];
+  startPage = 0;
+  endPage = 0;
+  params: {};
 
-  constructor(private dataservice: ProductService) { }
+  constructor(private router: Router, private route: ActivatedRoute , private dataservice: ProductService) { }
 
   ngOnInit() {
-    this.dataservice.getData()
-    .subscribe( data => {
-      this.products = data;
+    this.route.queryParams
+    .subscribe(params => {
+      if (JSON.stringify(params) !== JSON.stringify(this.params)) {
+        this.params = params;
+        this.dataservice.getData(this.params)
+        .subscribe(data => {
+          if (this.totalRecs !== data.totalRecs) {
+            this.endPage = 0;
+          }
+          this.products = data.products;
+          this.curPage = data.curPage;
+          this.totalPages = data.totalPages;
+          this.totalRecs = data.totalRecs;
+          this.pageLogic();
+        }, error => {
+          this.products = [];
+          this.curPage = 1;
+          this.totalPages = 1;
+          this.totalRecs = 0;
+          this.pageLogic();
+        }
+        );
+      }
     });
   }
 
+  pageLogic() {
+    if (this.curPage < this.startPage && this.curPage >= 0) {
+      this.navPages = [];
+      this.startPage = this.curPage - 4 > 0 ? this.curPage - 4 : 1;
+      this.endPage = this.startPage + 4 > this.totalPages ? this.totalPages : this.startPage + 4;
+      for (let i = this.startPage ; i <= this.endPage ; i++) {
+        this.navPages.push(i);
+      }
+    }
+
+    if (this.curPage > this.endPage && this.curPage <= this.totalPages) {
+      this.navPages = [];
+      this.endPage = this.curPage + 4 > this.totalPages ? this.totalPages : this.curPage + 4;
+      this.startPage = this.endPage - 4 > 0 ? this.endPage - 4 : 1;
+      for (let i = this.startPage ; i <= this.endPage ; i++) {
+        this.navPages.push(i);
+      }
+    }
+  }
+
+  searchParamChange(newParam) {
+    if (newParam.active === false) {
+      newParam.active = null;
+    }
+    this.router.navigate(['/products'], {queryParams: {...newParam}, queryParamsHandling: 'merge'});
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './../models/product.model';
 
@@ -7,18 +7,34 @@ import { Product } from './../models/product.model';
 })
 export class ProductService {
 
+  private curPageData: {};
   private url = 'http://localhost:3000/api/products';
 
+  // HTTP Options
   private httpOptions = {headers: new HttpHeaders({
       'x-auth-token':
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFudXNocmkiLCJmdWxsbmFtZSI6ImFudXNocmkgYXJvcmEiLCJyb2xlIjoidXNlciIsImlhdCI6MTU0NjE0MDc0OH0.a4NEIya-nJOhrAUKMgZvmVq8UHjE4pPJpzFULyxK_cM'
     })
   };
 
+  // Constructor
   constructor(private http: HttpClient) { }
 
-  getData () {
-    return this.http.get<Product[]>(this.url);
+  // HTTP GET
+  getData (inParams: {page?: number, name?: string, company?: string, active?: string}) {
+    const params = new HttpParams()
+        .set('page', (inParams && inParams.page ? inParams.page : 1).toString())
+        .set('name', inParams && inParams.name ? inParams.name : '')
+        .set('company', inParams && inParams.company ? inParams.company : '')
+        .set('active', inParams && inParams.active ? inParams.active : '');
+
+    return this.http.get<{
+      error: string,
+      totalRecs: number,
+      totalPages: number,
+      curPage: number,
+      products: Product[]}>
+      (this.url, {params: params});
   }
 
   createData (data: Product) {
@@ -38,4 +54,7 @@ export class ProductService {
     return this.http.delete(this.url + '/' + id, this.httpOptions );
   }
 
+  dataEmitter() {
+    // this.curPageData
+  }
 }
